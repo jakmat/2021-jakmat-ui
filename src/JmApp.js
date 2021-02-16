1d93281bebb0a35d872aa4e88adef2d298dea8ed
jakmat <jakubmatusiak87@gmail.com>
2021-02-16 23:29:41 +0100

    * [add] Sass * [refactor] CSS to SCSS

--- src/JmApp.js
+++ src/JmApp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './assets/jakmat-logo.png';
-import './JmApp.css';
+import './JmApp.scss';
 
 class JmApp extends Component {
   render() {
@@ -8,7 +8,7 @@ class JmApp extends Component {
       <div className="JmApp">
         <header className="JmApp-header">
           <img src={logo} className="JmApp-logo" alt="logo" />
-          <div class="JmApp-headline">
+          <div className="JmApp-headline">
             <h1 className="JmApp-title">Jakub Matusiak</h1>
             <h2 className="JmApp-subtitle">Geospatial Web Developer</h2>
           </div>
@@ -16,7 +16,7 @@ class JmApp extends Component {
         <nav className="JmApp-nav">
           {/*Navigation*/}
         </nav>
-        <main className="JmApp-body">
+        <main className="JmApp-main">
           {/*Content*/}
         </main>
         <footer className="JmApp-footer">
