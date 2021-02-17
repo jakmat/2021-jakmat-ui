5583df9321ea8cdbf82b368a58bd2f72a6e3c7f0
jakmat <jakubmatusiak87@gmail.com>
2021-02-17 23:57:20 +0100

    * [add] Content component outline

---
+++ src/app/shell/JmContent/JmContent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './JmContent.scss';
+
+class JmContent extends Component {
+  render() {
+    return (
+      <div className="JmContent"></div>
+    );
+  }
+}
+
+export default JmContent;
