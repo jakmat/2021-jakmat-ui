f2695e86c15cdf5040b1cc9d257f8903645ac614
jakmat <jakubmatusiak87@gmail.com>
2022-11-08 02:49:50 +0100

    Update lab deploy script

--- scripts/lab.deploy.sh
+++ scripts/lab.deploy.sh
@@ -1,9 +1,10 @@
 echo 'Starting lab deployment...'
 {
   npm run build:lab
+#  scp -r -P 222 ../build jakmat@s149.cyber-folks.pl:/home/jakmat/domains/jakubmatusiak.com/public_html/lab
   scp -r -P 222 ../build/index.html jakmat@s149.cyber-folks.pl:/home/jakmat/domains/jakubmatusiak.com/public_html/lab
   scp -r -P 222 ../build/static jakmat@s149.cyber-folks.pl:/home/jakmat/domains/jakubmatusiak.com/public_html/lab
-  #scp -r -P 222 ../build/assets jakmat@s149.cyber-folks.pl:/home/jakmat/domains/jakubmatusiak.com/public_html/lab
+#  scp -r -P 222 ../build/assets jakmat@s149.cyber-folks.pl:/home/jakmat/domains/jakubmatusiak.com/public_html/lab
   echo 'Lab deployment finished successfully.'
 } || {
   echo 'Lab deployment failed!'
